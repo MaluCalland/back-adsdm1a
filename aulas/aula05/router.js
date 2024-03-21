c653bfa27fac1075c70e7f784b5b41a428b976af
MaluCalland <malucalland@gmail.com>
2024-03-21 10:03:28 -0300

    codigo atual aula05

--- aulas/aula05/router.js
+++ aulas/aula05/router.js
@@ -2,37 +2,48 @@ const express = require('express');
 
 const router = express.Router();
 
+const produtos = [];
+
 router.get("/produtos", function(req, res){
-    res.json([]); //200 e JSON
+    res.json(produtos); //200 e JSON
 });
 
-router.get("/produtos/:produtoId", function(req, res){
-    if(req.params.produtoId == 1){
-        res.json({}); //200 e JOSN
-    } else {
-        res.status(404).json({msg: "Produto nao encontrado"});
-    }
-    res.json({}); //200 e JSON
-});
+router.get("/produtos/:produtoId", function(req, res) {
+    const encontrado = produtos.find(item => item.id == req.params.produtoId);
+    if (encontrado) {
+      res.json(encontrado);  //200 e JSON
+    } else {      res.status(404).json({msg: "Produto nao encontrado"})
+    }});
 
 router.post("/produtos", function(req, res){
     if(req.body && req.body.nome && req.body.preco){
-        res.status(201).json({});
+        const novo = {
+            id: produtos.length + 1,
+            nome: req.body.nome,
+            preco: req.body.preco
+        }
+        produtos.push(novo);
+        res.status(201).json(novo);
     } else {
-        res.status(422).json({msg: "Nome e/ou preco do produto obrigatórios"});
+        res.status(422).json({msg: "Nome e preco do produto obrigatórios"});
     }
 });
 
 router.put("/produtos/:produtoId", function(req, res){
-    if(req.params.produtoId == 1){
-        res.json({});
+    const encontrado = produtos.find(item => item.id == req.params.produtoId);
+    if (encontrado) {
+        encontrado.nome = req.body.nome;
+        encontrado.preco = req.body.preco;
+        res.json(encontrado);
     } else{
         res.status(404).json({msg: "Produto não encontrado"});
     }
 });
 
 router.delete("/produtos/:produtoId", function(req, res){
-    if(req.params.produtoId == 1){
+    const posicao = produtos.findIndex(item => item.id == req.params.produtoId);
+    if(posicao >= 0){
+        produtos.splice(posicao, 1);
         res.status(204).end();
     } else {
         res.status(404).json({msg: "Produto não encontrado"});
